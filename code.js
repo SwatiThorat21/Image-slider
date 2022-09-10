d3506d6043cf621ab5be3d9cce200a4a8e7c7709
Swati <swatic019@gmail.com>
2022-09-10 08:33:27 +0530

    stopwatch created

---
+++ code.js
@@ -0,0 +1,27 @@
+
+let flag = 0;
+
+function controller(x) {
+    flag += x;
+    slideshow(flag);
+
+}
+
+slideshow(flag);
+function slideshow(num) {
+    let slides = document.getElementsByClassName("mySlides");
+    if (num == slides.length) {
+        flag = 0;
+        num = 0;
+    }
+    if (num < 0) {
+        flag = slides.length-1;
+        num = slides.length-1;
+    }
+
+    for (let i of slides){
+        i.style.display = 'none';
+    }
+    slides[num].style.display = 'block';
+
+}
